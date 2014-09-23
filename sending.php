7f97668de8a7d6086308b429355b311510d876c0
adriandwiananto <bangateng@yahoo.com>
2014-09-23 11:39:58 +0700

    fix client poll

--- sending.php
+++ sending.php
@@ -24,26 +24,47 @@ $request = json_decode(file_get_contents("template.json"), true);
 $request["ASK"]["NIK"] = $id_number;
 $sendquery  = sendpost($CAaddr,$request);
 
-foreach ($sendquery as $key => $value) {
-    echo "Key: $key; Value: $value"; 
-}
-
 if ($sendquery["STATUS"]["Success"] == true) {
-    //catat query di file pid
     $pid = $sendquery["STATUS"]["PID"];
-    // $daftar = catatpid($IDNumber,$pid,$postdata);
-    //kirim data ke SI
-    // echo "Permintaan berhasil";
 }
 ?>
 
 <html>
     <head>
         <script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-        <script type="text/javascript" src="client.js"></script>
+        <!--<script type="text/javascript" src="client.js"></script>-->
+        <script type="text/javascript">
+            // function getContent(file_id,timestamp)
+            function getContent(timestamp)
+            {
+                var file_id = '<?php echo "$id_number.$pid"; ?>';
+
+                $.ajax(
+                    {
+                        type: 'GET',
+                        url: "http://localhost/ca/mobileid-CA/tanyaidentitas/poll-server.php?timestamp="+timestamp+"&file_id="+file_id,
+                        success: function(data){
+                            // put result data into "obj"
+                            var obj = jQuery.parseJSON(data);
+                            // put the data_from_file into #response
+                            $('#response').html(obj.data_from_file);
+                            // call the function again, this time with the timestamp we just got from server.php
+                            getContent(obj.timestamp);
+                        }
+                    }
+                );
+            }
+            // initialize jQuery
+            $(function() {
+                var pid = '<?php echo $id_number.$pid;?>';
+                // $('#response').html(pid);
+                // getContent(pid,0);
+                getContent(0);
+            });
+        </script>
     </head>
     <body>
         <h1>Response from server:</h1>
-        <div id="response"><?php echo "$id_number.$pid";?></div>
+        <div id="response"></div>
     </body>
 </html>
